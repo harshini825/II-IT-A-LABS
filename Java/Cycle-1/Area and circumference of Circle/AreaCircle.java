737e77bc2c41107554f040d6e8625de2c3214730
19wh1a1233 <69143132+harshini825@users.noreply.github.com>
2021-07-01 15:53:33 +0530

    Add files via upload

---
+++ Java/Cycle-1/Area and circumference of Circle/AreaCircle.java
@@ -0,0 +1,11 @@
+public class AreaCircle
+{
+public static void main(String[] args)
+{
+int r=13;
+double area = 3.14*r*r;
+double circum = 2*3.14*r;
+System.out.println("Area of circle = "+area);
+System.out.println("Circumference of circle = "+circum);
+}
+}
